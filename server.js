726f2e294ca96b3344a853e93814e7f281aacfe9
TASS154 <121037641+TASS154@users.noreply.github.com>
2024-09-30 08:37:56 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -18,6 +18,46 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/HTML/index.html')
 })
 
+app.get('/sobrenos', (req, res) => {
+    res.sendFile(__dirname + '/HTML/sobrenos.html')
+})
+
+app.get('/planos', (req, res) => {
+    res.sendFile(__dirname + '/HTML/planos.html')
+})
+
+app.get('/loja', (req, res) => {
+    res.sendFile(__dirname + '/HTML/loja.html')
+})
+
+app.get('/B-corp', (req, res) => {
+    res.sendFile(__dirname + '/HTML/B-corp.html')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/HTML/login.html')
+})
+
+app.get('/cursos', (req, res) => {
+    res.sendFile(__dirname + '/HTML/cursos.html')
+})
+
+app.get('/pp', (req, res) => {
+    res.sendFile(__dirname + '/HTML/pp.html')
+})
+
+app.get('/termos', (req, res) => {
+    res.sendFile(__dirname + '/HTML/termos.html')
+})
+
+
+app.get('/email', (req, res) => {
+    res.sendFile(__dirname + '/HTML/email.html')
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
