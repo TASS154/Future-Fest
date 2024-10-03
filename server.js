5bac5b303a3c4c06b1be9a0e5d48693e812341be
TASS154 <121037641+TASS154@users.noreply.github.com>
2024-10-03 11:14:48 -0300

    implementação do botão de exclusão

--- server.js
+++ server.js
@@ -1,207 +1,227 @@
-const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
-const app = express();
-const path = require('path')
-const bcrypt = require('bcrypt');
-const port = 3001;
-const methodOverride = require('method-override');
-const fs = require('fs');
+// Importa as dependências necessárias
+const express = require('express'); // Framework para criação de servidores web
+const { MongoClient, ObjectId } = require('mongodb'); // Cliente MongoDB para operações no banco de dados
+const app = express(); // Cria uma instância do aplicativo Express
+const path = require('path'); // Módulo para manipulação de caminhos de arquivos
+const bcrypt = require('bcrypt'); // Módulo para criptografia de senhas
+const port = 3000; // Porta em que o servidor irá escutar
+const methodOverride = require('method-override'); // Middleware para permitir métodos HTTP que não são suportados pelo HTML
+const fs = require('fs'); // Módulo para operações de sistema de arquivos
 
+// Middleware para lidar com requisições JSON e URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, '/HTML')));
+app.use(methodOverride('_method')); // Permite sobrepor métodos HTTP
+app.use(express.static(path.join(__dirname, '/HTML'))); // Serve arquivos estáticos da pasta HTML
 
-const url = 'mongodb://127.0.0.1:27017/';
-const dbName = 'academia';
-const collectionUser = 'usuarios';
-const collectionShop = 'loja';
+// Configuração do MongoDB
+const url = 'mongodb://127.0.0.1:27017/'; // URL de conexão ao MongoDB
+const dbName = 'academia'; // Nome do banco de dados
+const collectionUser = 'usuarios'; // Nome da coleção de usuários
+const collectionShop = 'loja'; // Nome da coleção de loja
 
+// Roteamento para as páginas HTML
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/HTML/home.html')
-})
+    res.sendFile(__dirname + '/HTML/home.html'); // Página inicial
+});
 
 app.get('/sobrenos', (req, res) => {
-    res.sendFile(__dirname + '/HTML/sobrenos.html')
-})
+    res.sendFile(__dirname + '/HTML/sobrenos.html'); // Página "Sobre Nós"
+});
 
 app.get('/planos', (req, res) => {
-    res.sendFile(__dirname + '/HTML/planos.html')
-})
+    res.sendFile(__dirname + '/HTML/planos.html'); // Página de Planos
+});
 
 app.get('/loja', (req, res) => {
-    res.sendFile(__dirname + '/HTML/dadossuplemento.html')
-})
+    res.sendFile(__dirname + '/HTML/loja.html'); // Página da Loja
+});
 
 app.get('/B-corp', (req, res) => {
-    res.sendFile(__dirname + '/HTML/B-corp.html')
-})
+    res.sendFile(__dirname + '/HTML/B-corp.html'); // Página B-corp
+});
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/HTML/login.html')
-})
+    res.sendFile(__dirname + '/HTML/login.html'); // Página de Login
+});
 
 app.get('/cursos', (req, res) => {
-    res.sendFile(__dirname + '/HTML/cursos.html')
-})
+    res.sendFile(__dirname + '/HTML/cursos.html'); // Página de Cursos
+});
 
 app.get('/pp', (req, res) => {
-    res.sendFile(__dirname + '/HTML/pp.html')
-})
+    res.sendFile(__dirname + '/HTML/pp.html'); // Página PP
+});
 
 app.get('/termos', (req, res) => {
-    res.sendFile(__dirname + '/HTML/termos.html')
-})
-
+    res.sendFile(__dirname + '/HTML/termos.html'); // Página de Termos
+});
 
 app.get('/email', (req, res) => {
-    res.sendFile(__dirname + '/HTML/email.html')
-})
+    res.sendFile(__dirname + '/HTML/email.html'); // Página de Email
+});
 
 app.get('/registro', (req, res) => {
-    res.sendFile(__dirname + '/HTML/registro.html')
-})
+    res.sendFile(__dirname + '/HTML/registro.html'); // Página de Registro
+});
 
 app.get('/conta', (req, res) => {
-    res.sendFile(__dirname + '/HTML/conta.html')
-})
-
-app.get('/planos', (req, res) => {
-    res.sendFile(__dirname + '/HTML/planos.html')
-})
-
-app.get('/esqueceu', (req, res) => {
-    res.sendFile(__dirname + '/HTML/esqueceu-a-senha.html')
-})
-
-app.get('/po', (req, res) => {
-    res.sendFile(__dirname + '/HTML/po.html')
-})
+    res.sendFile(__dirname + '/HTML/conta.html'); // Página de Conta
+});
 
 app.get('/dashboard', (req, res) => {
-    res.sendFile(__dirname + '/HTML/dashboard.html')
-})
+    res.sendFile(__dirname + '/HTML/dashboard.html'); // Página de Dashboard
+});
 
-app.post('/login', async (req, res) => {
-    const { email, senha } = req.body;
-
+app.get('/conta/:id', async (req, res) => {
     const client = new MongoClient(url);
-
+    const id = req.params.id; // Extrai o ID da URL
     try {
         await client.connect();
         const db = client.db(dbName);
         const collection = db.collection(collectionUser);
+
+        // Busca o usuário pelo ID
+        const usuario = await collection.findOne({ _id: new ObjectId(id) }); // Certifique-se de usar ObjectId
+
+        if (!usuario) {
+            return res.status(404).send('Usuário não encontrado'); // Retorna 404 se o usuário não existir
+        }
+
+        // Gera o HTML com os dados do usuário
+        const display = `
+        <div class="profile-img">
+            <img src="${usuario.foto}" alt="Imagem de Perfil" class="img-thumbnail" id="profileImage"
+                style="width: 200px; height: 200px;">
+        </div>
+        <div class="Seu-Perfil">
+            <div class="form-container">
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Vamos lá, mostre sua foto! (Aceitamos: PNG e JPG)</label>
+                    <input class="form-control input-sm" type="file" id="formFile">
+                </div>
+                <div class="mb-3">
+                    <label for="nomeUsuario" class="form-label">Nome de Usuário:</label>
+                    <div class="nome-container">
+                        <span id="nomeDisplay">${usuario.nome}</span> <!-- Use o campo correto do usuário -->
+                        <img src="https://i.ibb.co/DkQS76X/edit.png" alt="editar" class="edit" id="editIcon">
+                    </div>
+                    <input class="form-control input-sm" type="text" id="nomeInput" placeholder="Seu nome de usuário"
+                        style="display: none;">
+                    <button id="confirmButton" class="btn btn-primary" style="display: none;">Salvar</button>
+                </div>
+                <div class="mb-3 row align-items-center">
+                    <label for="staticEmail" class="col-auto col-form-label">Endereço de E-mail:<br>
+                        <span>${usuario.email}</span></label>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <form method="POST" action='/conta/${id}'>
+    <button type="submit" class="btn btn-danger btn-apagar">
+        <img src="https://i.ibb.co/Ss6dxLd/trashh.png" alt="Icone de apagar Conta" class="i-apagar">
+        <span class="btn-apagar-text">Apagar Conta</span>
+    </button>
+
+
+</form>
+        `;
+
+        res.send(display); // Envia o HTML gerado como resposta
+    } catch (err) {
+        console.error('Erro ao buscar usuário', err);
+        res.status(500).send('Erro ao buscar usuário, por favor, tente novamente mais tarde.');
+    } finally {
+        client.close(); // Fecha a conexão com o banco de dados
+    }
+});
+
+app.post('/conta/:id', async (req, res) => {
+    const id = req.params.id; // Obtém o ID da URL
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect(); // Conecta ao banco de dados
+        const db = client.db(dbName);
+        const collection = db.collection(collectionUser);
+
+        // Exclui o usuário pelo ID
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send('Usuário não encontrado'); // Retorna 404 se o usuário não existir
+        }
+
+        res.redirect('/'); // Retorna sucesso
+    } catch (err) {
+        console.error('Erro ao excluir usuário:', err); // Log do erro
+        res.status(500).send('Erro ao processar a requisição, por favor, tente novamente mais tarde.'); // Retorno de erro
+    } finally {
+        await client.close(); // Fecha a conexão com o banco de dados
+    }
+});
+
+// Rota para login
+app.post('/login', async (req, res) => {
+    const { email, senha } = req.body; // Extrai email e senha do corpo da requisição
+
+    const client = new MongoClient(url); // Cria uma nova instância do cliente MongoDB
+
+    try {
+        await client.connect(); // Conecta ao banco de dados
+        const db = client.db(dbName); // Seleciona o banco de dados
+        const collection = db.collection(collectionUser); // Seleciona a coleção de usuários
 
         // Procura o usuário pelo e-mail
         const user = await collection.findOne({ email });
 
         // Verifica se o e-mail existe
         if (!user) {
-            return res.status(401).send('E-mail não encontrado.'); // Email não registrado
+            return res.status(401).send('E-mail não encontrado.'); // Resposta se o email não foi encontrado
         }
 
-        console.log(user)
+        console.log(user); // Log do usuário encontrado
 
+        // Verifica a senha (aqui deveria ser usado bcrypt para comparar senhas por questão de segurança)
         if (senha !== user.senha) {
-            return res.status(401).send('Senha incorreta.'); // Senha incorreta
+            return res.status(401).send('Senha incorreta.'); // Resposta se a senha estiver incorreta
         } else {
-            res.redirect('/dashboard')
-        };
+            res.redirect('/dashboard'); // Redireciona para o dashboard se o login for bem-sucedido
+        }
     } catch (err) {
-        console.error('Erro ao fazer login', err);
-        res.status(500).send('Erro ao fazer login, por favor, tente novamente mais tarde.');
+        console.error('Erro ao fazer login', err); // Log de erro
+        res.status(500).send('Erro ao fazer login, por favor, tente novamente mais tarde.'); // Resposta em caso de erro
     } finally {
-        client.close();
+        client.close(); // Fecha a conexão com o banco de dados
     }
 });
 
+// Rota para registro de novos usuários
 app.post('/registro', async (req, res) => {
-    const newUser = req.body
-    const client = new MongoClient(url)
+    const newUser = req.body; // Extrai os dados do novo usuário do corpo da requisição
+    const client = new MongoClient(url); // Cria uma nova instância do cliente MongoDB
 
     try {
-        await client.connect();
+        await client.connect(); // Conecta ao banco de dados
 
-        const db = client.db(dbName);
-        const collection = db.collection(collectionUser)
+        const db = client.db(dbName); // Seleciona o banco de dados
+        const collection = db.collection(collectionUser); // Seleciona a coleção de usuários
 
-        const result = await collection.insertOne(newUser)
-        console.log(`usuário ${result.insertedId} inserido com sucesso`)
+        // Insere o novo usuário na coleção
+        const result = await collection.insertOne(newUser);
+        console.log(`usuário ${result.insertedId} inserido com sucesso`); // Log do ID do usuário inserido
 
-        res.redirect('/')
+        res.redirect('/'); // Redireciona para a página inicial
     } catch (err) {
-        console.error('Erro ao cadastrar usuario', err)
-        res.status(500).send('Erro ao fazer o regristro da conta, por favor, tente novamente mais tarde')
+        console.error('Erro ao cadastrar usuario', err); // Log de erro
+        res.status(500).send('Erro ao fazer o registro da conta, por favor, tente novamente mais tarde'); // Resposta em caso de erro
     } finally {
-        client.close();
+        client.close(); // Fecha a conexão com o banco de dados
     }
-
-})
-
-// saida do banco de dados para cards
-
-
-function criarCard(suplemento) {
-    let cards = '';
-    suplemento.forEach(suplemento => {
-        cards += `
-        <div class="card mb-3">
-            <img src="/images/${suplemento.img}" class="card-img-top w-100 h-auto" alt="${suplemento.nome}">
-            <div class="card-body">
-                <h5 class="card-title">${suplemento.nome}</h5>
-                <p class="card-text">${suplemento.desc}</p>
-                <p class="card-text"><strong>Preço:</strong> R$ ${suplemento.preco}</p>
-                <a href="${suplemento.url_info}" class="btn btn-primary">Mais Informações</a>
-            </div>
-        </div>
-        `;
-    });
-    return cards;
-}
-function criarCard(suplemento) {
-    let cards = '';
-    suplemento.forEach(suplemento => {
-        cards += `
-        <div class="card mb-3">
-            <img src="/images/${suplemento.imagem}" class="card-img-top w-100 h-auto" alt="${suplemento.nome}">
-            <div class="card-body">
-                <h5 class="card-title">${suplemento.nome}</h5>
-                <p class="card-text">${suplemento.desc}</p>
-                <p class="card-text"><strong>Preço:</strong> R$ ${suplemento.preco}</p>
-                <a href="${suplemento.url_info}" class="btn btn-primary">Mais Informações</a>
-            </div>
-        </div>
-        `;
-    });
-    return cards;
-}
-function criarCard(suplemento) {
-    let cards = '';
-    suplemento.forEach(suplemento => {
-        cards += `
-        <div class="card mb-3">
-            <img src="/images/${suplemento.imagem}" class="card-img-top w-100 h-auto" alt="${suplemento.nome}">
-            <div class="card-body">
-                <h5 class="card-title">${suplemento.nome}</h5>
-                <p class="card-text">${suplemento.desc}</p>
-                <p class="card-text"><strong>Preço:</strong> R$ ${suplemento.preco}</p>
-                <a href="${suplemento.url_info}" class="btn btn-primary">Mais Informações</a>
-            </div>
-        </div>
-        `;
-    });
-    return cards;
-}
-
-
-app.get('/', async (req, res) => {
-    const cardsHtml = suplemento.map(suplemento => criarCard(suplemento)).join('');
-    const pageHtmlPath = path.join(__dirname, 'dadossuplemento.html');
-    let pageHtml = fs.readFileSync(pageHtmlPath, 'utf-8');
-    pageHtml = pageHtml.replace('{{cardsHtml}}', cardsHtml);
-    res.send(pageHtml);
 });
 
+// Inicia o servidor na porta especificada
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+    console.log(`http://localhost:${port}`); // Log para indicar que o servidor está rodando
+});
